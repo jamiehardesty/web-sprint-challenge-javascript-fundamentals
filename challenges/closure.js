30e964bb8ad182f5027af9f4c658aaed35461a52
Jamie Hardesty <jamiehardesty@outlook.com>
2020-08-21 09:24:17 -0700

    t2 counter - closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -21,6 +21,15 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
-// function summation(param){
-//   
-// }
+function summation(counter){
+  let arr = [];
+  let sum = 0;
+  let param = 0;
+  for (let i = 0; i < counter; i++){
+    sum += param++;
+    arr.push("These are to be added together: " + param);
+  }
+  arr.push("This is the total return matching Number('ten') as specified: " + (sum += param));
+  return arr;
+}
+console.log(summation(4)); // 1, 2, 3, 4 = 10 
