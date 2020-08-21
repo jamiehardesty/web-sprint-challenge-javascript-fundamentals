17a6d2633a2c1e82bf47755ad4d0072aac966ae5
Jamie Hardesty <jamiehardesty@outlook.com>
2020-08-21 08:47:42 -0700

    r1 - ar/cb

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,7 +20,10 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = []; // animal_name & scientific_name 
+zooAnimals.forEach(function(item){
+  return displayNames.push("Name: " + item.animal_name + ", Scientific: " + item.scientific_name);
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +32,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = 0;
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,7 +40,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = 0;
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
